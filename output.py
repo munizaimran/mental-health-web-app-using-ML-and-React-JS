4b7ce1ef2440ca92e0e133fee869ad0022ae713d
Muniza Imran <49648357+munizaimran@users.noreply.github.com>
2020-12-23 23:25:06 +0500

    Update output.py

--- output.py
+++ output.py
@@ -1,56 +1,26 @@
-from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
-import pickle
-import numpy as np
-from model import NLPModel
+from flask import Flask, render_template, Response
+
 
 app = Flask(__name__)
-api = Api(app)
+@app.route('/')
+def index():
+    # rendering webpage
+    return render_template('index.html')
 
-model = NLPModel()
+import real_time_video
+#os.system('python real_time_video.py')
 
-clf_path = 'lib/models/SentimentClassifier.pkl'
-with open(clf_path, 'rb') as f:
-    model.clf = pickle.load(f)
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-vec_path = 'lib/models/TFIDFVectorizer.pkl'
-with open(vec_path, 'rb') as f:
-    model.vectorizer = pickle.load(f)
-
-# argument parsing
-parser = reqparse.RequestParser()
-parser.add_argument('query')
-
-
-class PredictSentiment(Resource):
-    def get(self):
-        # use parser and find the user's query
-        args = parser.parse_args()
-        user_query = args['query']
-
-        # vectorize the user's query and make a prediction
-        uq_vectorized = model.vectorizer_transform(np.array([user_query]))
-        prediction = model.predict(uq_vectorized)
-        pred_proba = model.predict_proba(uq_vectorized)
-
-        # Output either 'Negative' or 'Positive' along with the score
-        if prediction == 0:
-            pred_text = 'Negative'
-        else:
-            pred_text = 'Positive'
-
-        # round the predict proba value and set to new variable
-        confidence = round(pred_proba[0], 3)
-
-        # create JSON object
-        output = {'prediction': pred_text, 'confidence': confidence}
-
-        return output
-
-
-# Setup the Api resource routing here
-# Route the URL to the resource
-api.add_resource(PredictSentiment, '/')
+@app.route('/your_face')
+def your_face():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # defining server ip address and port
+    app.run(host='0.0.0.0',port='5000', debug=True)
